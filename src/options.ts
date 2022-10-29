1e621c32592dbbf14da975bd155a5c4e2d05df74
David Valin <hola@davidvalin.com>
2022-10-30 00:53:10 +0200

    Options + package.json

---
+++ src/options.ts
@@ -0,0 +1,26 @@
+export const options = {
+  'mail': [
+    'sendmail'
+  ],
+  'chat': [
+    'matrix'
+  ],
+  'whiteboard': [
+    'whitebophir',
+    'spacedeck-open'
+  ],
+  'cloud': [
+    'owncloud'
+  ],
+  'git': [
+    'gitea',
+    'gitlab'
+  ],
+  'cicd': [
+    'jenkins',
+    'drone.io'
+  ],
+  'backups': [
+    ''
+  ]
+}
